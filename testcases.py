123d84b90ba115a3b377e6ddfab13d67963287ba
stefan.r <stefan.r@551886.no-reply.drupal.org>
2018-05-07 22:19:13 +0100

    cowboy

--- testcases.py
+++ testcases.py
@@ -8,3 +8,16 @@ class RobotTestCase(unittest.TestCase):
         self.robot = new Robot(0, 0, 'N')
         self.assertEqual(self.robot.orientation, 0)
         self.assertEqual(self.robot.get_orientation(), 'N')
+        self.assertEqual(self.robot.get_position(), Position(0,0))
+
+class SimulationTestCase(unitest.TestCase):
+    def setUp(self):
+        self.robot = new Robot(0, 0, 'N')
+        self.simulation = new Simulation(self.robot)
+
+    def testSimulation(self):
+        self.assertEqual(self.robot.get_orientation(), 'N')
+        self.assertEqual(self.robot.get_position(), Position(0,0))
+        self.simulation.send_command("F")
+        self.assertEqual(self.robot.get_orientation(), 'N')
+        self.assertEqual(self.robot.get_position(), Position(0,1))
